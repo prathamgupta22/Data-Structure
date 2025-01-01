f33770853b4091266df73a19b6391e9e79b4c930
Pratham22102001 <pratham.guptapg22@gmail.com>
2025-01-01 21:11:00 +0530

    done 2 more ques

--- Basic/Pattern/Patten.java
+++ Basic/Pattern/Patten.java
@@ -1,6 +1,6 @@
 public class Patten {
   public static void main(String[] args) {
-    q5(5);
+    q8(5);
   }
 
   public static void q1(int n) {
@@ -15,7 +15,7 @@ public class Patten {
   public static void q2(int n) {
     for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
-        System.out.print("*");
+        System.out.print("*" + " ");
       }
       System.out.println();
     }
@@ -49,15 +49,36 @@ public class Patten {
   }
 
   public static void q6(int n) {
-
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
   }
 
   public static void q7(int n) {
-
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < 2 * i + 1; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 
   public static void q8(int n) {
-
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < 2 * n - (2 * i + 1); k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 
   public static void q9(int n) {
@@ -69,6 +90,22 @@ public class Patten {
   }
 
   public static void q11(int n) {
+
+  }
+
+  public static void q12(int n) {
+
+  }
+
+  public static void q13(int n) {
+
+  }
+
+  public static void q14(int n) {
+
+  }
+
+  public static void q15(int n) {
 
   }
 }
